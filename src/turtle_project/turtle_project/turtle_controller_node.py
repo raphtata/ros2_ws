8d678f5d3faf1feb573f960e6284d1020e55ac2f
raphtata <raph.tata@gmail.com>
2024-03-14 15:42:31 +0100

    add turtle service to kill one  when it reachs goal

--- src/turtle_project/turtle_project/turtle_controller_node.py
+++ src/turtle_project/turtle_project/turtle_controller_node.py
@@ -3,11 +3,13 @@
 import rclpy
 import math 
 from rclpy.node import Node
-from example_interfaces.msg import String
 from turtlesim.msg import Pose 
+from functools import partial
 from rclpy.qos import ReliabilityPolicy, QoSProfile
 from geometry_msgs.msg import Twist
-
+from my_robot_interfaces.msg import TurtleInfo
+from my_robot_interfaces.msg import TurtleAlive
+from my_robot_interfaces.srv import CatchTurtle
 
 class TurtleController(Node):
 
@@ -17,14 +19,15 @@ class TurtleController(Node):
         # the parameter you pass is the node name
         super().__init__('turtle_controller_node')
         self.pose_ = None
-        self.x_target = 8.0 
-        self.y_target = 4.0
+
+        self.turtle_to_catch_ = None
         self.distPtTr_ = 0.0
         ##subscriber##
         self.subscriber = self.create_subscription(Pose,'/turtle1/pose',
         self.listener_callback,
-        QoSProfile(depth=10, reliability=ReliabilityPolicy.RELIABLE))  # is the most used to read LaserScan data and some sensor data.
+        QoSProfile(depth=10, reliability=ReliabilityPolicy.RELIABLE)) 
         self.publisher_ = self.create_publisher(Twist, '/turtle1/cmd_vel', 10)
+        self.subscriber = self.create_subscription(TurtleAlive, 'alive_turtles', self.turtle_alive_callback, 10)
         self.control_loop_timer = self.create_timer(0.01, self.control_callback)
     
     def angular (self , x2, x1 , y2 , y1):
@@ -35,16 +38,21 @@ class TurtleController(Node):
         alpha = math.atan2 (disY , disX)
         return alpha
     
+    def turtle_alive_callback(self,msg):
+
+        if len(msg.turtles) > 0:
+            self.turtle_to_catch_ = msg.turtles[0]
+
     def control_callback(self):
 
-        if self.pose_ == None : 
+        if self.pose_ == None or self.turtle_to_catch_ == None: 
             return 
                 
         self.get_logger().info("subscriber msg y : " + str(self.pose_.y) + " x :" + str(self.pose_.x))
 
         msg_to_send = Twist()
             
-        angle_dest = self.angular(self.x_target, self.pose_.x , self.y_target, self.pose_.y)
+        angle_dest = self.angular(self.turtle_to_catch_.x, self.pose_.x , self.turtle_to_catch_.y , self.pose_.y)
         self.get_logger().info("send angular cmd : " + str(angle_dest))
         angular_diff = angle_dest - self.pose_.theta
 
@@ -63,9 +71,31 @@ class TurtleController(Node):
             msg_to_send.linear.x  = 0.0
             msg_to_send.angular.z = 0.0
             self.get_logger().info("stop to run")
+            self.turtle_to_remove(self.turtle_to_catch_.name)
         
         self.publisher_.publish(msg_to_send)
+    
+    def turtle_to_remove(self, name):
 
+        client = self.create_client(CatchTurtle, "catch_service")
+            
+        while not client.wait_for_service(1.0):
+            self.get_logger().info( "wait for server")
+        
+        request = CatchTurtle.Request()
+        request.name = name
+
+        #get the result 
+        future = client.call_async(request)
+        future.add_done_callback(partial(self.call_back_result, name = request.name))
+        
+    def call_back_result(self, future, name):
+        try : 
+            result = future.result()
+            if result : 
+                self.get_logger().info("Turtle " + str(name) + " has been deleted " )
+        except Exception as e:
+            self.get_logger().info("service call failed %r" %(e,))
 
     def listener_callback(self,msg):
         self.pose_ = msg
---
+++ src/turtle_project/turtle_project/turtle_spawner_node.py
@@ -0,0 +1,111 @@
+#! /usr/bin/env python3
+
+import rclpy
+import random
+from rclpy.node import Node
+from functools import partial
+from my_robot_interfaces.msg import TurtleInfo
+from my_robot_interfaces.msg import TurtleAlive
+from my_robot_interfaces.srv import CatchTurtle
+
+from turtlesim.srv import Spawn
+from turtlesim.srv import Kill
+
+class TurtleSpawner(Node):
+
+    def __init__(self):
+        # Here you have the class constructor
+        # call super() in the constructor to initialize the Node object
+        # the parameter you pass is the node name
+        super().__init__('turtle_spawn_node')
+        self.timer_period = 4.0
+        self.turtle_count_ = 0
+        self.timer_pawner= self.create_timer(self.timer_period, self.timer_spawner_callback)
+        self.server = self.create_service(
+            CatchTurtle, "catch_service" , self.turtle_service_callback)
+        self.turtle_list_ = []
+        self.turtle_alive_publisher_ = self.create_publisher(TurtleAlive, 'alive_turtles', 10)
+        self.timer = self.create_timer(self.timer_period, self.turtles_update)
+    
+    def turtles_update(self): 
+        msg = TurtleAlive()
+        msg.turtles = self.turtle_list_ 
+        self.turtle_alive_publisher_.publish(msg)
+
+    def turtle_service_callback(self, request, response):
+        turtle_to_catch = request.name
+        #delete name in the list
+        self.turtle_list_  = [turtle for turtle in self.turtle_list_ if turtle.name != turtle_to_catch ]
+        #kill turtle node 
+        self.kill_turtle(request.name)
+        self.turtles_update()
+        #for turtle in self.turtle_list_: 
+        #    if turtle.name == turtle_to_catch :
+        #        del self.turtle_list_.remove(turtle)
+        response.success = True
+        return response
+
+    def kill_turtle(self, name): 
+        client = self.create_client(Kill, "kill" )
+            
+        while not client.wait_for_service(1.0):
+            self.get_logger().info( "wait for server")
+        
+        request = Kill.Request()
+        request.name = name
+
+        #get the result 
+        future = client.call_async(request)
+        future.add_done_callback(partial(self.kill_turtle_node_callback, name = request.name))
+
+    def kill_turtle_node_callback(self,future, name) :
+        try : 
+            result = future.result()
+            if result : 
+                self.get_logger().info("Turtle " + str(name) + " has been deleted from node list " )
+        except Exception as e:
+            self.get_logger().info("service call failed %r" %(e,))
+
+    def timer_spawner_callback(self):
+
+        self.turtle_count_ += 1 
+        client = self.create_client(Spawn, "spawn" )
+            
+        while not client.wait_for_service(1.0):
+            self.get_logger().info( "wait for server")
+        
+        request = Spawn.Request()
+        request.x = random.uniform(0.0, 10.0)
+        request.y = random.uniform(0.0, 10.0) 
+        request.theta = random.uniform(0.0, 3.4) 
+        request.name = "coco" + str(self.turtle_count_)
+        turtle = TurtleInfo()
+        turtle.name = request.name 
+        turtle.x = request.x 
+        turtle.y = request.y 
+        turtle.theta = request.theta 
+
+        self.turtle_list_.append(turtle)
+        #get the result 
+        future = client.call_async(request)
+        future.add_done_callback(partial(self.call_back_result, name = request.name))
+    
+    def call_back_result(self, future, name):
+        try : 
+            result = future.result()
+            if result : 
+                self.get_logger().info("Turtle " + str(name) + " has been created " )
+        except Exception as e:
+            self.get_logger().info("service call failed %r" %(e,))
+
+
+def main(args=None):
+
+    rclpy.init(args=args)
+    mynode = TurtleSpawner()
+    rclpy.spin(mynode)
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
